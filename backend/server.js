dc51a931dde51b5051426402c7af8436f4502015
Adarsh Chavhan <adarshchavhan@outlook.com>
2022-10-11 10:34:12 +0530

    cors origin

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 const connectDb = require('./config/db');
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
 dotenv.config({path:'backend/config/config.env'});
 connectDb();
